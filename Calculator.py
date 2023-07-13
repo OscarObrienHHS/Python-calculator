c88409915824f199b2b38ae8b511b9d41bdd26f6
OscarObrienHHS <95391126+OscarObrienHHS@users.noreply.github.com>
2023-07-13 14:42:13 +1200

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,4 @@
+"""This is a python calculator."""
 import math
 
 # setting up stuff for checking that the shape is valid
@@ -5,18 +6,22 @@ validshapes = ["rectangle", "circle", "triangle", "parallelogram"]
 calculations = open("storedcalculations.txt", "a+")
 # Importing math for the square root (sqrt) in herons formula
 
+
 # Error Checker thingy majig
-def getFloat(msg):
+def getfloat(msg):
+    """Check if input is valid."""
     try:
         f = float(input(msg))
+        # if the number is less than or equal to zero the code below will run
         if f <= 0:
             print('Must be bigger then 0 you dumbass')
-            return getFloat(msg)
+            return getfloat(msg)
         return f
     except ValueError:
         # This will run if the input is not a number
         print("You didnt enter a number >:(, Youre a horrible person")
-        return getFloat(msg)
+        return getfloat(msg)
+
 
 # Menu for the user to select what they want to do
 ans = True
@@ -28,11 +33,11 @@ while ans:
     3: Clear History
     4: Exit program
     """)
-    ans=input("What would you like to do? \n") 
+    ans = input("What would you like to do? \n")
     # if the user enters 1 the code below will run
-    if ans=="1": 
-        print("Calculator Opening") 
-      # Get the Shape the user wants to calculate and set it to lowercase.
+    if ans == "1":
+        print("Calculator Opening")
+        # Get the Shape the user wants to calculate and set it to lowercase.
         shape = input('Please Select a following shape by typing it: Rectangle, Circle, Triangle, Parallelogram \n')
         shape = shape.lower()
 
@@ -45,8 +50,8 @@ while ans:
         # If the Shape is a Rectangle this code will run.
         if shape == validshapes[0]:
             # Get the length and width of the rectangle.
-            length = getFloat('What is the length of your rectangle?\n')
-            width = getFloat('What is the width of your rectangle?\n')
+            length = getfloat('What is the length of your rectangle?\n')
+            width = getfloat('What is the width of your rectangle?\n')
             # Calculate the area and perimeter
             area = width * length
             perimeter = 2 * (length + width)
@@ -59,7 +64,7 @@ while ans:
         # If the Shape is a Circle this code will run.
         if shape == validshapes[1]:
             # Getting the radius of the Circle
-            radius = getFloat('What is the radius of your circle?\n')
+            radius = getfloat('What is the radius of your circle?\n')
             # Setting pi variable
             pi = 3.14159265359
             # Calculating
@@ -72,7 +77,7 @@ while ans:
             calculations.flush()
 
         # If the shape is a Triangle this code will run.
-        if shape == validshapes[2]: 
+        if shape == validshapes[2]:
             # A menu for the user to select their dimensions
             triangle_ans = True
             while triangle_ans:
@@ -81,16 +86,16 @@ while ans:
                 1: All sides (Herons formula)
                 2: Base + Height
                 """)
-                triangle_ans=input("What would you like to do? \n") 
+                triangle_ans = input("What would you like to do? \n")
                 # If they enter one this code will run.
-                if triangle_ans=="1":
+                if triangle_ans == "1":
                     # Getting the three sides of the triangle
-                    side1 = getFloat('What is the first side of your triangle?\n')
-                    side2 = getFloat('What is the second side of your triangle?\n')
-                    side3 = getFloat('What is the third side of your triangle?\n')
+                    side1 = getfloat('What is the first side of your triangle?\n')
+                    side2 = getfloat('What is the second side of your triangle?\n')
+                    side3 = getfloat('What is the third side of your triangle?\n')
                     # Calculatiing the area and perimeter
                     perimeter = side1 + side2 + side3
-                    s = perimeter / 2 
+                    s = perimeter / 2
                     area = math.sqrt(s * (s - side1) * (s - side2) * (s - side3))
                     # Printing out the answer for the user
                     print('The area of your {} is {} and your perimeter is {}'.format(shape, area, perimeter))
@@ -101,12 +106,12 @@ while ans:
                     triangle_ans = False
                     # If they enter two this code will run.
 
-                elif triangle_ans=="2":
+                elif triangle_ans == "2":
                     # Getting the base and height
-                    base = getFloat('What is the base of your triangle?\n')
-                    height = getFloat('What is the height of your triangle?\n')
+                    base = getfloat('What is the base of your triangle?\n')
+                    height = getfloat('What is the height of your triangle?\n')
                     # Calculating the area
-                    area = (height * base) / 2 
+                    area = (height * base) / 2
                     # Printing out the answer for the user
                     print('The area of your {} is {} and you cannot get a perimeter from only base and height'.format(shape, area))
                     # Writing information into the calculations file
@@ -122,9 +127,9 @@ while ans:
         # If the shape is a Parallelogram this code will run and calculate the area and perimeter of the shape.
         if shape == validshapes[3]:
             # Getting the dimensions of a Parallelogram
-            base = getFloat('What is length of the base of your parallelogram? \n')
-            height = getFloat('What is height of your parallelogram? \n')
-            side = getFloat('What is the length of the side of your parallelogram?\n')
+            base = getfloat('What is length of the base of your parallelogram? \n')
+            height = getfloat('What is height of your parallelogram? \n')
+            side = getfloat('What is the length of the side of your parallelogram?\n')
             # Calculating the area and perimeter
             area = base * height
             perimeter = 2 * (side + base)
@@ -135,20 +140,20 @@ while ans:
             calculations.flush()
 
     # If the user enters 2 the code below will run.
-    elif ans=="2":
+    elif ans == "2":
         print("\nHistory Printing")
         # Set cursor to the top of the file and then read and print it out for the user
         calculations.seek(0)
         print(calculations.read())
 
     # If the user enters 3 the code below will run.
-    elif ans=="3":
-        print("\nHistory Cleared") 
+    elif ans == "3":
+        print("\nHistory Cleared")
         # This code will remove all of the text from the file.
         calculations.truncate(0)
 
     # If the user enters 4 the code below will run.
-    elif ans=="4":
+    elif ans == "4":
         # Printing out the history before the program closes
         print("\nHistory Printing")
         # Set cursor to the top of the file and then read and print it out for the user
@@ -157,10 +162,8 @@ while ans:
         # Printing a goodbye message and closing the calculations file
         print("\nGoodbye")
         quit()
-        calculations.close() 
-    
+        calculations.close()
+
     # This code will run if the user didnt enter a number between 1-4
     else:
-        print("\nNot Valid Choice Try again") 
-
-
+        print("\nNot Valid Choice Try again")
